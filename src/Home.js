e8d416b2f3fccff2463785a46bf708c2586339c1
Isaac Chong <ichong1996.ic@gmail.com>
2021-03-28 18:34:11 +0800

    Lesson 7 completed

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log("Hello there", e);
+    }
+
+    const handleSecondClick = (name, e) => {
+        console.log("Hello there! General " + name + "!", e.target);
+    }
+
     return (  
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick = {handleClick}>Click Me</button>
+            <button onClick = {(e) => handleSecondClick("Kenobi", e)}>Click me for weird stuff</button>
         </div>
     );
 }
